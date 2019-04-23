d55857858686c339272454b1826d4144b48f6da1
Rahman <rachman.sd@gmail.com>
2019-04-23 09:55:49 +0700

    create form for submit topic

--- src/components/DiggView.vue
+++ src/components/DiggView.vue
@@ -3,7 +3,16 @@
         <div class="row">
             <div class="col-sm-5 col-sm-offset-3 col-md-6 col-md-offset-3">
                 <div class="row">
-                    
+                    <div class="col-sm-12">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="title">
+                        </div>
+                        <div class="form-group">
+                            <textarea class="form-control" rows="5" placeholder="topic content"></textarea>
+                        </div>
+                        <span>0 / 255</span>
+                        <button class="btn btn-info pull-right" >Create Topic</button>
+                    </div>
                 </div>
                 <div class="row" style="margin-top: 20px">
                     
