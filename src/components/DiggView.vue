46d77082f275225aadd015890efc866d0570f342
Rahman <rachman.sd@gmail.com>
2019-04-23 10:04:07 +0700

    button action to submit form

--- src/components/DiggView.vue
+++ src/components/DiggView.vue
@@ -5,13 +5,13 @@
                 <div class="row">
                     <div class="col-sm-12">
                         <div class="form-group">
-                            <input type="text" class="form-control" placeholder="title">
+                            <input type="text" class="form-control" placeholder="title" v-model="topic.title">
                         </div>
                         <div class="form-group">
-                            <textarea class="form-control" rows="5" placeholder="topic content"></textarea>
+                            <textarea class="form-control" rows="5" placeholder="topic content" v-model="topic.content"></textarea>
                         </div>
                         <span>0 / 255</span>
-                        <button class="btn btn-info pull-right" >Create Topic</button>
+                        <button class="btn btn-success pull-right" @click="tosubmit">Create Topic</button>
                     </div>
                 </div>
                 <div class="row" style="margin-top: 20px">
@@ -21,3 +21,31 @@
         </div>
     </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      topic: { title: '', content: '' },
+      topics: []
+    }
+  },
+  methods: {
+    tosubmit () {
+      // check if topic title is empty
+      if(this.topic.title.length === 0) {
+        alert('title can`t empty')
+        return
+      }
+      // check if topic content is empty
+      if(this.topic.content.length === 0) {
+        alert('topic conent can`t empty')
+        return
+      }
+      // push topic list
+      this.topics.push(Object.assign({ upvote: 0, downvote: 0 }, this.topic))
+      // empty form content
+      this.topic = { title: '', content: '' }
+    }
+  }
+}
+</script>
