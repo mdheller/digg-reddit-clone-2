ba0645d71aecb1c598eb6fdfa1b159d3651c5987
Rahman <rachman.sd@gmail.com>
2019-04-23 11:34:04 +0700

    create method post on back-end side

--- server.js
+++ server.js
@@ -2,5 +2,9 @@ var express = require('express');
 var serveStatic = require('serve-static');
 const app = express();
 app.use(serveStatic(__dirname + "/dist"));
+app.post('/create/topic', (req, res) => {
+    console.log(req.body)
+    res.json({success: true})
+})
 var port = process.env.PORT || 5000;
 app.listen(port);
